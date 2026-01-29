ce046bff27f2f523fc098667c62b7e02e6898c2d
Kazuma-hubsys <ando-kazuma158@g.ecc.u-tokyo.ac.jp>
2026-01-29 15:04:18 +0900

    biom-hydrogen capex succeed

--- myconfig/config.py
+++ myconfig/config.py
@@ -11,6 +11,7 @@ cepci_dict = load_csv_as_dict(DATA_DIR / "general" / "CEPCI.csv")
 plant_data = load_csv(DATA_DIR / "general" / "plant_parameter.csv")
 commodity_data = load_csv(DATA_DIR / "general" / "commodity_price.csv")
 equipment_costs = load_csv(DATA_DIR / "general" / "equipment_cost_parameter.csv")
+
 general_data = ["cepci_dict", "plant_data", "commodity_data", "equipment_costs"]
 
 # infra
@@ -18,10 +19,12 @@ general_data = ["cepci_dict", "plant_data", "commodity_data", "equipment_costs"]
 pipe_data = load_csv(DATA_DIR / "infra" / "pipeline_parameter.csv")
 truck_data = load_csv(DATA_DIR / "infra" / "truck_parameter.csv")
 equipment_data = load_csv(DATA_DIR / "infra" / "equipment_parameter.csv")
+
 infra_data = ["pipe_data", "truck_data", "equipment_data"]
 
 # each process
 steel_data = load_csv(DATA_DIR / "steel_production.csv")
+
 process_data = ["steel_data"]
 
 # CCS
@@ -38,13 +41,20 @@ awe_data = load_csv(DATA_DIR / "electrolysis" / "AWE_parameter.csv")
 pemwe_data = load_csv(DATA_DIR / "electrolysis" / "PEMWE_parameter.csv")
 awe_adv_data = load_csv(DATA_DIR / "electrolysis" / "AWE_adv_parameter.csv")
 pemwe_adv_data = load_csv(DATA_DIR / "electrolysis" / "PEMWE_adv_parameter.csv")
+
 we_data = ["awe_data", "pemwe_data", "awe_adv_data", "pemwe_adv_data"]
 
 # Biomass
 bio_infra_assump_data = load_csv(DATA_DIR / "biomass" / "biomass_infra_assumption.csv")
 bio_gather_cost_data = load_csv(DATA_DIR / "biomass" / "biomass_gathering_cost.csv")
-bio_capex_data = load_csv(DATA_DIR / "biomass_cost.csv")
-bio_hydrogen_requirement_data = load_csv(DATA_DIR / "biomass_requirement_hydrogen.csv")
-biomass_data = ["bio_infra_assump_data", "bio_gather_cost_data", "bio_capex_data", "bio_hydroen_requirement_data"]
+bio_capex_data = load_csv(DATA_DIR / "biomass" / "biomass_cost.csv")
+bio_hydrogen_requirement_data = load_csv(DATA_DIR / "biomass" / "biomass_requirement_hydrogen.csv")
+
+biomass_data = ["bio_infra_assump_data", "bio_gather_cost_data", "bio_capex_data", "bio_hydrogen_requirement_data"]
+
+
+#########
+## all ##
+#########
 
 __all__ = general_data + infra_data + process_data + ccs_data + we_data + biomass_data
